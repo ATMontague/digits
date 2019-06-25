ac332263f6d67e19ebda1c515f3a1b5824836689
adam <atmontague10@gmail.com>
2019-06-25 14:54:05 -0700

    playing around with images. made folders for images

---
+++ image_touchup.py
@@ -0,0 +1,27 @@
+import os
+from PIL import Image
+
+image_path = '/home/adam/Documents/kaggle/digits/generated_images/'
+new_path = '/home/adam/Documents/kaggle/digits/cleaned_images/'
+
+black = (0,0,0)
+white = (255, 255, 255)
+threshold=  (160, 160, 160) # how to determine threshold?
+
+for filename in os.listdir(image_path):
+    # open image and get pixels
+    img = Image.open(image_path+filename).convert('LA')
+    pixels = img.getdata()
+
+    new_pixels = []
+    for pixel in pixels:
+        if pixel < threshold:
+            new_pixels.append(black)
+        else:
+            new_pixels.append(white)
+
+    # create and save new image
+    new = Image.new('RGB', img.size)
+    new.putdata(new_pixels)
+    name = new_path + filename[:-3] + 'test.png'
+    new.save(name)
