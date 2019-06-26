cf26003e8687a9ed9ce60a768ad004baeca7b089
adam <atmontague10@gmail.com>
2019-06-26 11:42:01 -0700

    moved around code to playaround easier

--- image_touchup.py
+++ image_touchup.py
@@ -8,20 +8,24 @@ black = (0,0,0)
 white = (255, 255, 255)
 threshold=  (160, 160, 160) # how to determine threshold?
 
-for filename in os.listdir(image_path):
-    # open image and get pixels
-    img = Image.open(image_path+filename).convert('LA')
-    pixels = img.getdata()
+def convert():
+    for filename in os.listdir(image_path):
+        # open image and get pixels
+        img = Image.open(image_path+filename).convert('LA')
+        pixels = img.getdata()
 
-    new_pixels = []
-    for pixel in pixels:
-        if pixel < threshold:
-            new_pixels.append(black)
-        else:
-            new_pixels.append(white)
+        new_pixels = []
+        for pixel in pixels:
+            if pixel < threshold:
+                new_pixels.append(black)
+            else:
+                new_pixels.append(white)
 
-    # create and save new image
-    new = Image.new('RGB', img.size)
-    new.putdata(new_pixels)
-    name = new_path + filename
-    new.save(name)
+        # create and save new image
+        new = Image.new('RGB', img.size)
+        new.putdata(new_pixels)
+        name = new_path + filename
+        new.save(name)
+
+if __name__ == '__main__':
+    convert()
